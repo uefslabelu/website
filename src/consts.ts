0eb08fd4abf133adb533157966a5aab572c88b78
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-04-15 22:22:38 -0300

    site: change site constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Labelu'
+export const SITE_DESCRIPTION = `
+    Laboratório de História e Memória da Esquerda e das Lutas Sociais
+`
