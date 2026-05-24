56e8c1fc7a56d838c8910696d7e4ad4a9e254f45
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-05-24 00:01:39 -0300

    Fix spacing around dot

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,5 @@
 
 export const REPO = "pj-labelu"
 export const SITE_TITLE = 'Labelu'
-export const SITE_DESCRIPTION = `
-    Laboratório de História e Memória da Luta de Classes
-`
-export const COPYRIGHT_NAME = `
-    Labelu - Laboratório de História e Memória da Luta de Classes
-`
+export const SITE_DESCRIPTION = "Laboratório de História e Memória da Luta de Classes"
+export const COPYRIGHT_NAME = "Labelu - Laboratório de História e Memória da Luta de Classes"
