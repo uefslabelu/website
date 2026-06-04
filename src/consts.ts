3a23a56038c547b004aceb9ebaa395124a634846
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2026-06-04 18:13:27 -0300

    Use fleuron as divisors

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const REPO = "pj-labelu"
-export const SITE_TITLE = 'Labelu ✦ Laboratório de História e Memória da Luta de Classes'
+export const SITE_TITLE = 'Labelu 🙡 Laboratório de História e Memória da Luta de Classes'
 export const SITE_DESCRIPTION = `
 Somos, o nome diz, um laboratório — um espaço para o exercício de um trabalho (labor) intelectual: a pesquisa histórica. Existimos há mais de 21 anos fazendo pesquisa e ajudando a formar bons pesquisadores em História.
 Nosso objetivo principal é pesquisar a história e preservar a memória das lutas sociais e da esquerda. Nossos estudos dialogam intensamente com a História Social, investigando sujeitos, estratégias, memórias e os antagonismos que se defrontam nas lutas de classes.
